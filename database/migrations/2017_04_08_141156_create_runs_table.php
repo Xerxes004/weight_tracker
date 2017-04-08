5e8d606271c051d88647a18244ddfd208940ddbf
Wes Kelly <weskelly@mac.com>
2017-04-08 15:30:15 -0400

    Add migration for runs table.

---
+++ database/migrations/2017_04_08_141156_create_runs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('runs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->tinyInteger('dur_min')->default(0);
+            $table->tinyInteger('dur_sec')->default(0);
+            $table->decimal('dist_mi', 5, 2)->default(0.00);
+            $table->mediumInteger('cals')->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('runs');
+    }
+}
