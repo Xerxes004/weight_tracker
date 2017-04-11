e64c5749cbc00761c1fc1a298f2c35a6be2e1453
Wes Kelly <weskelly@mac.com>
2017-04-10 22:31:29 -0400

    Add id to run data table

--- resources/views/runs/all.blade.php
+++ resources/views/runs/all.blade.php
@@ -1,17 +1,19 @@
-<table>
-	<tr><th colspan="5">Past Run Data</th></tr>
+<table id="run_info_table">
+	<tr><th colspan="6">Past Run Data</th></tr>
 	<tr>
 		<th>Runtime</th>
 		<th>Distance</th>
 		<th>Cals Burned</th>
+		<th>Average Speed</th>
 		<th></th>
 		<th></th>
 	</tr>
 @forelse ( $runs as $run )
 	<tr>
 		<td>{{ $run->dur_min }}:{{ str_pad($run->dur_sec, 2, 0) }}</td>
-		<td>{{ $run->dist_mi }}</td>
-		<td>{{ $run->cals }}</td>
+		<td>{{ $run->dist_mi }} mi</td>
+		<td>{{ $run->cals }} kcal</td>
+		<td>{{ $run->mph() }} mph</td>
 		<td><a href="">update</a></td>
 		<td><a href="/runs/delete/{{ $run->id }}">delete</a></td>
 	</tr>
