b9a390b9efe67d286915dacf43f109b2b7897cbe
Wes Kelly <weskelly@mac.com>
2017-04-10 22:29:07 -0400

    Add helper functions for run time data

--- app/Run.php
+++ app/Run.php
@@ -7,4 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Run extends Model
 {
   protected $table = 'runs';
+
+  public function mph() {
+    return round(($this->dist_mi / $this->dur_hrs()), 2);
+  }
+
+  public function dur_hrs() {
+    return round(($this->dur_min/60 + $this->dur_sec/(3600)), 2);
+  }
+
+  public function dur_mins() {
+    return round(($this->dur_min + $this->dur_sec/60), 2);
+  }
+
+  public function dur_sec() {
+    return ($this->dur_min * 60 + $this->dur_sec);
+  }
 }
